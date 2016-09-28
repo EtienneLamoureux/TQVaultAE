38f71d2679e29fce6ddbd5fd3b85a3ce4d77b901
Etienne Lamoureux <etienne.lamoureux@crystalgorithm.com>
2016-09-27 22:02:34 -0400

    Remove commented out code in MainForm.cs

--- TQVault/MainForm.cs
+++ TQVault/MainForm.cs
@@ -213,18 +213,6 @@ namespace TQVault
 			SetupGamePaths();
 			SetupMapName();
 
-			// Check if an updated version is available.
-			//if (Settings.Default.CheckForNewVersions)
-			//{
-			//    UpdateDialog updateDialog = new UpdateDialog();
-			//    updateDialog.ShowUpToDateMessage = false;
-			//    updateDialog.CheckForUpdates();
-			//    if (updateDialog.DialogResult == DialogResult.OK || updateDialog.DialogResult == DialogResult.Cancel)
-			//    {
-			//        updateDialog.Close();
-			//    }
-			//}
-
 			// Setup localized strings.
 			this.characterLabel.Text = Resources.MainFormLabel1;
 			this.vaultLabel.Text = Resources.MainFormLabel2;
@@ -343,73 +331,6 @@ namespace TQVault
 		protected override void ScaleForm(float scaleFactor, bool useRelativeScaling)
 		{
 			base.ScaleForm(scaleFactor, useRelativeScaling);
-
-			/*float thisScale;
-
-            // Check if we are resetting the size.
-            if (scaleFactor == 1.0F)
-            {
-                if (Database.DB.Scale == 1.0F)
-                {
-                    return;
-                }
-
-                thisScale = this.OriginalFormScale;
-
-                // Reset the border graphics to the originals.
-                this.topBorder = Resources.BorderTop;
-                this.bottomBorder = Resources.BorderBottom;
-                this.sideBorder = Resources.BorderSide;
-                this.bottomRightCorner = Resources.BorderBottomRightCorner;
-                this.bottomLeftCorner = Resources.BorderBottomLeftCorner;
-
-                isFromResize = false;
-            }
-            else
-            {
-                thisScale = scaleFactor;
-            }
-
-            // Support relative resizing of the DB value.
-            if (isFromResize)
-            {
-                float newDBScale = Database.DB.Scale * scaleFactor;
-                if (newDBScale > 2.0F || newDBScale < 0.40F)
-                {
-                    return;
-                }
-
-                Database.DB.Scale = newDBScale;
-                this.Scale(new SizeF(scaleFactor, scaleFactor));
-            }
-            else if (scaleFactor == 1.0F)
-            {
-                Database.DB.Scale = thisScale;
-
-                // Use the width since it is usually more drastic of a change.
-                // especially when coming from a small size.
-                this.Scale(new SizeF(
-                    (float)this.OriginalFormSize.Width / (float)this.Width,
-                    (float)this.OriginalFormSize.Width / (float)this.Width));
-
-                Settings.Default.Scale = 1.0F;
-                Settings.Default.Save();
-            }
-            else
-            {
-                float scalingWidth = (float)this.OriginalFormSize.Width / (float)this.Width * thisScale;
-                float scalingHeight = (float)this.OriginalFormSize.Height / (float)this.Height * thisScale;
-                float scaling = scalingWidth;
-
-                // Use the scaling factor closest to one.
-                if ((scalingHeight < scalingWidth && scalingHeight > 1.0F) || (scalingHeight > scalingWidth && scalingHeight < 1.0F))
-                {
-                    scaling = scalingHeight;
-                }
-
-                Database.DB.Scale = thisScale;
-                this.Scale(new SizeF(scaling, scaling));
-            }*/
 
 			this.itemText.Text = string.Empty;
 
