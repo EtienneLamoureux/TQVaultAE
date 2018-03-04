c999adcfea20fb69975893420f7a7109e521d9fe
Emerson Pinter <dev@pinter.com.br>
2018-03-04 17:53:47 +0000

    Add style to ScalingCombobox
    Use dark background and light fonts, with light border.

--- src/TQVaultAE.GUI/ScalingComboBox.cs
+++ src/TQVaultAE.GUI/ScalingComboBox.cs
@@ -5,6 +5,7 @@
 //-----------------------------------------------------------------------
 namespace TQVaultAE.GUI
 {
+	using System;
 	using System.Drawing;
 	using System.Windows.Forms;
 
@@ -13,6 +14,18 @@ namespace TQVaultAE.GUI
 	/// </summary>
 	public class ScalingComboBox : ComboBox
 	{
+		private static int WM_PAINT = 0x000F;
+
+		public ScalingComboBox()
+		{
+			this.FlatStyle = FlatStyle.Popup;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(31)))), ((int)(((byte)(21)))));
+			this.ForeColor = System.Drawing.Color.White;
+			this.DrawMode = DrawMode.OwnerDrawFixed;
+			this.DrawItem += ScalingComboboxDrawItem;
+			this.Font = Program.GetFontAlbertusMTLight(8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+		}
+
 		/// <summary>
 		/// Reverts the basic settings of a control back to the original settings.
 		/// </summary>
@@ -20,11 +33,12 @@ namespace TQVaultAE.GUI
 		/// <param name="size">New Size of the control</param>
 		public void Revert(Point location, Size size)
 		{
-			this.Font = new Font("Microsoft Sans Serif", 8.25F);
 			this.Location = location;
 			this.Size = size;
-			this.BackColor = SystemColors.Window;
-			this.ForeColor = SystemColors.WindowText;
+			this.FlatStyle = FlatStyle.Popup;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(31)))), ((int)(((byte)(21)))));
+			this.ForeColor = System.Drawing.Color.White;
+			this.Font = Program.GetFontAlbertusMTLight(8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 		}
 
 		/// <summary>
@@ -37,6 +51,39 @@ namespace TQVaultAE.GUI
 			this.Font = new Font(this.Font.FontFamily, this.Font.SizeInPoints * factor.Height, this.Font.Style);
 
 			base.ScaleControl(factor, specified);
+		}
+
+		private void ScalingComboboxDrawItem(object sender, DrawItemEventArgs e)
+		{
+			var combo = sender as ComboBox;
+
+			if (e.Index < 0) return;
+			if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+			{
+				e.Graphics.FillRectangle(new SolidBrush(System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(195)))), ((int)(((byte)(112)))))), e.Bounds);
+			}
+			else
+			{
+				e.Graphics.FillRectangle(new SolidBrush(this.BackColor), e.Bounds);
+			}
+			e.Graphics.DrawString(combo.Items[e.Index].ToString(),
+										  e.Font,
+										  new SolidBrush(Color.White),
+										  new Point(e.Bounds.X, e.Bounds.Y));
+
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+
+			Graphics g = Graphics.FromHwnd(Handle);
+
+			if (m.Msg == WM_PAINT)
+			{
+				Rectangle bounds = new Rectangle(0, 0, Width, Height);
+				ControlPaint.DrawBorder(g, bounds, Color.FromArgb(223, 188, 97), ButtonBorderStyle.Solid);
+			}
 		}
 	}
 }
