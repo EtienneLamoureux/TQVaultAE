09dff76d5ec1ca24fb06fd0299bf7d188cb5e321
Ninakoru <ninakoru@gmail.com>
2019-05-18 15:38:05 +0200

    Downgrade Assembly version to 3.0.0

--- src/TQVaultAE.GUI/Properties/AssemblyInfo.cs
+++ src/TQVaultAE.GUI/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // CLS compliant attribute
