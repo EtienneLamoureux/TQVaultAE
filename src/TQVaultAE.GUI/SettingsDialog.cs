dccd907b35eb262a4329191985f8fc38d05be447
s0da72 <s0da72@yahoo.com>
2019-06-04 18:26:33 -0500

    oops now it does

--- src/TQVaultAE.GUI/SettingsDialog.cs
+++ src/TQVaultAE.GUI/SettingsDialog.cs
@@ -232,7 +232,7 @@ namespace TQVaultAE.GUI
 			{
 				this.allowItemEditCheckBox.Visible = false;
 				this.allowItemCopyCheckBox.Visible = false;
-				this.allowItemCopyCheckBox.Visible = false;
+				this.characterEditCheckBox.Visible = false;
 			}
 		}
 
