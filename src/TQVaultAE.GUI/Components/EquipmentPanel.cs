ee4fdaa2302f1aaa1a36ab2873f929106bfb8e1a
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2020-04-15 11:05:25 -0400

    Revert "Proposed fix for #304 (#305)"
    This reverts commit a629cd25e372a9d310e0df904bf95a55af333711.

--- src/TQVaultAE.GUI/Components/EquipmentPanel.cs
+++ src/TQVaultAE.GUI/Components/EquipmentPanel.cs
@@ -444,11 +444,7 @@ namespace TQVaultAE.GUI.Components
 						}
 
 						dragItem.Location = SackCollection.GetEquipmentLocationOffset(slot);
-						
-						if (itemUnderUs != null)
-							// Only remove something if there is something to remove.
-							this.Sack.RemoveAtItem(slot);
-							
+						this.Sack.RemoveAtItem(slot);
 						this.Sack.InsertItem(slot, dragItem);
 					}
 				}
